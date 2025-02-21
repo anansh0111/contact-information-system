186a6e676223f95b345a9399fc0e93732b70e88e
anansh0111 <142689253+anansh0111@users.noreply.github.com>
2025-02-21 21:29:38 +0530

    Create Command.java

---
+++ Command.java
@@ -0,0 +1,8 @@
+public class Command {
+    public static final String ADD_CONTACT = "ADD_CONTACT";
+    public static final String GET_CONTACTS = "GET_CONTACTS";
+    public static final String DELETE_CONTACT = "DELETE_CONTACT";
+    public static final String UPDATE_CONTACT = "UPDATE_CONTACT";
+    public static final String SEARCH_CONTACT = "SEARCH_CONTACT";
+}
+
